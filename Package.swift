211796304e7654db4b4247e26cef9a28ccdce3eb
Jack Mousseau <jackmousseau@gmail.com>
2025-12-12 16:33:33 -0800

    Bump swift-syntax version 602.0.0

--- Package.swift
+++ Package.swift
@@ -27,7 +27,7 @@ let package = Package(
         ),
         .package(
             url: "https://github.com/swiftlang/swift-syntax",
-            "600.0.0" ..< "602.0.0"
+            from: "602.0.0"
         )
     ],
     targets: [
